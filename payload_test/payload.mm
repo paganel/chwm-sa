39f13c495907a3d1c98c85c30579d8db1cbc27d9
koekeishiya <aasvi93@hotmail.com>
2018-08-01 22:16:45 +0200

    update spaces creation/removal for latest Mojave Beta

--- payload_test/payload.mm
+++ payload_test/payload.mm
@@ -218,8 +218,8 @@ static void init_instances()
                 remove_space_fp = remove_space_addr;
             }
         } else if (os_version.minorVersion == 14) {
-            add_space_fp = baseaddr + 0x296a80;
-            remove_space_fp = baseaddr + 0x3d4620;
+            add_space_fp = baseaddr + 0x27ef80;
+            remove_space_fp = baseaddr + 0x37fbd0;
         }
 
         managed_space = objc_getClass("Dock.ManagedSpace");
