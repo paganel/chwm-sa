ceaf2d10ea4327b76d6672ece3f6e4cfe6300255
koekeishiya <aasvi93@hotmail.com>
2019-04-03 21:42:11 +0200

    cleanup injector code

--- inject_test/main.m
+++ inject_test/main.m
@@ -1,54 +1,15 @@
 #import <Cocoa/Cocoa.h>
-#import <Foundation/Foundation.h>
 #import <ScriptingBridge/ScriptingBridge.h>
-
-#include <stdio.h>
-#include <string.h>
-
-@interface Controller : NSObject <SBApplicationDelegate> { }
-
-- (void) eventDidFail:(const AppleEvent *)Event withError:(NSError *)Error;
-- (void) Inject;
-
-@end
-
-@implementation Controller
-
-- (void) eventDidFail:(const AppleEvent *)Event withError:(NSError *)Error
-{
-    return;
-}
-
-- (void) Inject
-{
-    for(NSRunningApplication *Application in [[NSWorkspace sharedWorkspace] runningApplications])
-    {
-        pid_t PID = Application.processIdentifier;
-        const char *Name = [[Application localizedName] UTF8String];
-        if(Name)
-        {
-            if(strcmp(Name, "Dock") == 0)
-            {
-                printf("process found\n");
-
-                SBApplication *SBApp = [SBApplication applicationWithProcessIdentifier:PID];
-
-                [SBApp setTimeout:10*60];
-                [SBApp setSendMode:kAEWaitReply];
-                [SBApp sendEvent:'ascr' id:'gdut' parameters:0];
-                [SBApp setSendMode:kAENoReply];
-                [SBApp sendEvent:'CHWM' id:'injc' parameters:0];
-
-                printf("event sent\n");
-            }
-        }
-    }
-}
-
-@end
 
 int main(int Count, char **Args)
 {
-    Controller *_Controller = [[Controller alloc] init];
-    [_Controller Inject];
+    SBApplication *SBApp = [[SBApplication applicationWithBundleIdentifier:@"com.apple.Dock"] retain];
+    if (SBApp == nil) return -1;
+
+    [SBApp setTimeout:10*60];
+    [SBApp setSendMode:kAEWaitReply];
+    [SBApp sendEvent:'ascr' id:'gdut' parameters:0];
+    [SBApp setSendMode:kAENoReply];
+    [SBApp sendEvent:'CHWM' id:'injc' parameters:0];
+    [SBApp release];
 }
